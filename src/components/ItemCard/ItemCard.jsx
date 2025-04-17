74b3bd11316ae2edd8f8ac140d0273a990affbad
alexiscruz1403 <jr.alexis.cruz.2003@gmail.com>
2025-04-17 14:30:39 -0300

    Se creo la logica de almacenamiento en localStorage, el componente itemCard y la logica para cambiar el estado de las peliculas almacenadas

---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,45 @@
+import './ItemCard.css';
+import { Star, Clapperboard, Calendar, TvMinimalPlay, User } from 'lucide-react';
+
+const ItemCard = ({ titulo, director, anio, genero, tipo, rating, onStateChange, watched }) => {
+
+    const handleOnClick = (e) => {
+        onStateChange({ titulo, director, anio, genero, tipo, rating });
+    }
+
+    return (
+        <div className="item-card">
+            <h3 className="item-title">{titulo}</h3>
+            <div className="item-info">
+                <User size="16" fill="#4b5563"></User>
+                <p><strong>Director:</strong> {director}</p>
+            </div>
+            <div className="item-info">
+                <Calendar size="16" fill="#3b82f6"></Calendar>
+                <p><strong>Año:</strong> {anio}</p>
+            </div>
+            <div className="item-info">
+                <Clapperboard size="16" fill="#f472b6"></Clapperboard>
+                <p><strong>Género:</strong> {genero}</p>
+            </div>
+            <div className="item-info">
+                <TvMinimalPlay size="16" fill="#34d399"></TvMinimalPlay>
+                <p><strong>Tipo:</strong> {tipo}</p>
+            </div>
+            <div className="item-info">
+                <Star size="16" fill="#facc15"></Star>
+                <p className="item-rating">Rating:{rating}</p>
+            </div>
+            <div className="item-options">
+                {watched 
+                ? <button className="item-button button-watched" onClick={handleOnClick}>Por ver</button> 
+                : <button className="item-button button-watched" onClick={handleOnClick}>Vista</button>
+                }
+                <button className="item-button button-edit">Editar</button>
+                <button className="item-button button-delete">Eliminar</button>
+            </div>
+        </div>
+    );
+}
+
+export default ItemCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Title } from '../../components/Title/Title';
 import { List } from '../../components/List/List';
 import { Star } from 'lucide-react';
@@ -8,23 +8,58 @@ import './Home.css';
 
 const titlePage = "Gestor de Películas y Series";
 
-const forSeeList = {
-    subtitle: "Por ver",
-    sentence: "Añade una serie o película.",
-    empty: true,
-    icon: <Play fill="lightgreen" strokeWidth={1} size={24} />
-};
-
-const viewList = {
-    subtitle: "Vistas",
-    sentence: "Aún no has añadido una pelicula o serie como vista.",
-    empty: true,
-    icon: <Star fill="yellow" strokeWidth={0.5} size={24} />
-};
-
 export const Home = () => {
-
+    //localStorage.clear();
     const [search, setSearch] = useState('');
+
+    const [toWatchList, setToWatchList] = useState(JSON.parse(localStorage.getItem('toWatchList')) || []);
+    const [watchedList, setWatchedList] = useState(JSON.parse(localStorage.getItem('watchedList')) || []);
+
+    useEffect(() => {
+        localStorage.setItem('toWatchList', JSON.stringify(toWatchList));
+    }, [toWatchList]);
+
+    useEffect(() => {
+        localStorage.setItem('watchedList', JSON.stringify(watchedList));
+    }, [watchedList]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+        const toWatchList = JSON.parse(localStorage.getItem('toWatchList')) || [];
+        toWatchList.push(data);
+        setToWatchList(toWatchList);
+        e.target.reset();
+    };
+
+    const changeItemState = (item) => {
+        const toWatchList = JSON.parse(localStorage.getItem('toWatchList')) || [];
+        const watchedList = JSON.parse(localStorage.getItem('watchedList')) || [];
+
+        if(toWatchList.some((i) => i.titulo === item.titulo)){
+            setToWatchList(toWatchList.filter((i) => i.titulo !== item.titulo));
+            setWatchedList([...watchedList, item]);
+        }else{
+            setWatchedList(watchedList.filter((i) => i.titulo !== item.titulo));
+            setToWatchList([...toWatchList, item]);
+        }
+    }
+
+    const toWatchListProps = {
+        subtitle: "Por ver",
+        sentence: "Añade una serie o película.",
+        itemsList: toWatchList,
+        icon: <Play fill="lightgreen" strokeWidth={1} size={24} />,
+        stateChangeAction: changeItemState
+    };
+    const watchedListProps = {
+        subtitle: "Vistas",
+        sentence: "Aún no has añadido una pelicula o serie como vista.",
+        itemsList: watchedList,
+        icon: <Star fill="yellow" strokeWidth={0.5} size={24} />,
+        stateChangeAction: changeItemState
+    };
 
     return (
         <div className="container">
@@ -62,31 +97,31 @@ export const Home = () => {
             {/* Listas */}
 
             <div className="listas">
-                <List list={forSeeList} /> {/* renderiza las pelis por ver */}
-                <List list={viewList} /> // {/* renderiza las pelis ya vistas. */}
+                <List list={toWatchListProps} /> //{/* renderiza las pelis por ver */}
+                <List list={watchedListProps} /> {/*//  renderiza las pelis ya vistas.*/}
             </div>
 
             {/* Formulario */}
             <div className="formulario">
                 <h2>Agregar nueva película o serie</h2>
-                <form>
-                    <input className="input" type="text" placeholder="Título" required />
-                    <input className="input" type="text" placeholder="Director" required />
-                    <input className="input" type="number" placeholder="Año" required />
+                <form onSubmit={handleSubmit}>
+                    <input className="input" name="titulo" id="titulo" type="text" placeholder="Título" required/>
+                    <input className="input" name="director" id="director" type="text" placeholder="Director" required/>
+                    <input className="input" name="anio" id="anio" type="number" placeholder="Año" min="0" max="2025" required/>
 
-                    <select className="select" required>
+                    <select className="select" name="genero" id="genero">
                         <option value="">Género</option>
-                        <option value="drama">Drama</option>
-                        <option value="comedia">Comedia</option>
-                        <option value="acción">Acción</option>
+                        <option value="Drama">Drama</option>
+                        <option value="Comedia">Comedia</option>
+                        <option value="Acción">Acción</option>
                     </select>
 
-                    <input className="input" type="number" placeholder="Rating (1-10)" min="1" max="10" />
+                    <input className="input" name="rating" id="rating" type="number" placeholder="Rating (1-10)" min="1" max="10" required/>
 
-                    <select className="select" required>
+                    <select className="select" name="tipo" id="tipo">
                         <option value="">Tipo</option>
-                        <option value="pelicula">Película</option>
-                        <option value="serie">Serie</option>
+                        <option value="Pelicula">Película</option>
+                        <option value="Serie">Serie</option>
                     </select>
 
                     <button className="button" type="submit">Agregar</button>
